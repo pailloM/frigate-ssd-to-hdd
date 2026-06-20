7372907a678e79fef536dfba920f37a50aca9fd8
PailloM <paillomams@gmx.com>
2026-06-20 09:04:41 -0400

    more logging

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,15 @@ set -euo pipefail
 CRON_SCHEDULE="${CRON_SCHEDULE:-0 2 * * *}"
 MIN_AGE_DAYS="${MIN_AGE_DAYS:-0}"
 
+echo "Adding job to cron"
+
 echo "$CRON_SCHEDULE MIN_AGE_DAYS=$MIN_AGE_DAYS /usr/local/bin/ssd-to-hdd.sh sync >> /proc/1/fd/1 2>&1" > /etc/crontabs/root
 
-echo "Will start sync in 3 minutes."
+echo "Will start initial sync in 3 minutes."
 sleep 180
 
 /usr/local/bin/ssd-to-hdd.sh sync
+
+echo "Starting cron scheduler"
 
 exec crond -f -l 2
